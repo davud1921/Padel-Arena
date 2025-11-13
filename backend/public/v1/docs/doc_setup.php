44005c580b1fa1c7c195196ba438be5096752f55
davud1921 <davud.mahmutovic@stu.ibu.edu.ba>
2025-11-13 14:42:57 +0100

    swagger

---
+++ backend/public/v1/docs/doc_setup.php
@@ -0,0 +1,28 @@
+<?php
+/**
+* @OA\Info(
+*     title="Padel Arena API",
+*     description="Backend API for Padel Arena booking system",
+*     version="1.0",
+*     @OA\Contact(
+*         email="web2001programming@gmail.com",
+*         name="Padel Arena Backend Support"
+*     )
+* )
+*/
+
+/**
+* @OA\Server(
+*     url= "http://localhost/Padel-Arena/backend",
+*     description="Local development server"
+* )
+*/
+
+/**
+* @OA\SecurityScheme(
+*     securityScheme="ApiKey",
+*     type="apiKey",
+*     in="header",
+*     name="Authentication"
+* )
+*/
