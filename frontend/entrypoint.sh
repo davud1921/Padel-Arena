12fceb85c43099472c6bb6526aa7ff3ae76dfb74
Davud Mahmutovic <davud.mahmutovic@stu.ibu.edu.ba>
2025-12-29 04:54:19 +0100

    fix

--- frontend/entrypoint.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-set -e
-
-# ubaci PORT u nginx config
-envsubst '${PORT}' < /etc/nginx/templates/default.conf.template > /etc/nginx/conf.d/default.conf
-
-# start nginx
-nginx -g 'daemon off;'
